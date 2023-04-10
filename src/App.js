69af2e5b0035897b9ef618ef78ed6b39194397d1
Mohammad-Shiyyab <mohammadshiyab1996@gmail.com>
2023-04-10 20:23:38 +0000

    17

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from '../src/component/home/Home'
+import { Route , Routes } from 'react-router-dom';
+import Footer from '../src/component/Footer/Footer'
+import Nav from '../src/component/Navbar/nav';
+import Header from '../src/component/Header/Header';
 
+import TourDetails from './component/TourDetails/TourDetails';
 function App() {
   return (
     <div >
-      <Home />
+      <Nav />
+      <Header />
+      <Routes>
+<Route path='/' element={<Home />}></Route>
+<Route path='/city/:id' element={<TourDetails />}></Route>
+
+      </Routes>
+      <Footer />
     </div>
   );
 }
